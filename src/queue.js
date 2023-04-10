033b132cc1371cc284bff6fb8efc03df8c402c70
VladzimirBely <vladzimir.bely@gmail.com>
2023-04-10 18:53:21 +0300

    feat: add resolve for stack and 66% for queue

---
+++ src/queue.js
@@ -0,0 +1,37 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+// const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+
+class Queue {
+  constructor() {
+    this.buffer = [];
+  }
+
+  getUnderlyingList() {
+    
+  }
+
+  enqueue(value) {
+    this.buffer.push(value);
+  }
+
+  dequeue() {
+    return this.buffer.shift();
+  }
+}
+
+module.exports = {
+  Queue
+};
---
+++ src/stack.js
@@ -0,0 +1,35 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Implement the Stack with a given interface via array.
+ *
+ * @example
+ * const stack = new Stack();
+ *
+ * stack.push(1); // adds the element to the stack
+ * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ * stack.pop(); // returns the top element from stack and deletes it, returns 1
+ * stack.pop(); // undefined
+ *
+ */
+class Stack {
+  constructor() {
+    this.buffer = [];
+  }
+  
+  push(element) {
+    this.buffer.push(element);
+  }
+
+  pop() {
+    return this.buffer.pop();
+  }
+
+  peek() {
+    return this.buffer[this.buffer.length - 1];
+  }
+}
+
+module.exports = {
+  Stack
+};
